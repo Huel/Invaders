07c22c378e1cc357db991bac3352f3fa91c85538
svenddutz <svend.dutz@wooga.com>
2014-07-25 22:07:05 +0200

    adding crosshair and raycast to hit enemies, BOOM!

---
+++ Invaders/Assets/GunControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunControl : MonoBehaviour {
+
+	public Camera myCam;
+
+	void Start () 
+	{
+	}
+	
+	void Update () 
+	{
+		RaycastHit hit;
+		if(Input.GetButtonDown("Fire1"))
+		{
+			if(Physics.Raycast(myCam.transform.position, myCam.transform.forward,out hit))
+			{
+				if(hit.collider.gameObject.tag == "enemy")
+				{
+					Debug.Log("BOOM");
+					Destroy(hit.collider.gameObject);
+				}
+			}
+		}
+	}
+}
