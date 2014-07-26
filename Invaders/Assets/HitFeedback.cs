bd5ec6a27ef41a2a24ad70565bd800a2d7c25377
antonlicht <anton.licht@gmx.de>
2014-07-26 20:29:10 +0200

    add hitfeedback

---
+++ Invaders/Assets/HitFeedback.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HitFeedback : MonoBehaviour
+{
+
+    private Image _image;
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void Hit()
+    {
+        Color color = _image.color;
+        color.a = 1f;
+        _image.color = color;
+        gameObject.SetActive(true);
+
+    }
+
+    void Update()
+    {
+
+        if (_image.color.a > 0)
+            {
+                Color color = _image.color;
+                color.a -= Time.deltaTime * 1f;
+                _image.color = color;
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+    }
+}
