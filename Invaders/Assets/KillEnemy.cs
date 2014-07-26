ef6f1b2800061a342cd642c6880b05d77a168781
antonlicht <anton.licht@gmx.de>
2014-07-26 17:58:32 +0200

    add particles

---
+++ Invaders/Assets/KillEnemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillEnemy : MonoBehaviour
+{
+    public GameObject Particles;
+
+    private SpriteRenderer _renderer;
+    private bool _kill;
+
+    void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        Kill();
+    }
+
+    public void Kill()
+    {
+        Particles.transform.parent = null;
+        Particles.SetActive(true);
+        _kill = true;
+    }
+
+    void Update()
+    {
+        if (_renderer.color.a > 0)
+        {
+            Color color = _renderer.color;
+            color.a -= Time.deltaTime*1f;
+            _renderer.color = color;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
