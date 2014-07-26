7a53fd4c90694cf890ecb289e68cc409c1b2230d
antonlicht <anton.licht@gmx.de>
2014-07-26 16:43:52 +0200

    add first enemy

---
+++ Invaders/Assets/LookAtPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    private Transform _player;
+    void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	void Update () 
+    {
+        transform.LookAt(_player);
+	}
+}
